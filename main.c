c045c91594026aebbc370104cd7c34ef0bf0af04
Virus.V <virusv@live.com>
2025-02-25 09:51:49 +0800

    remove unused code

--- main.c
+++ main.c
@@ -93,45 +93,6 @@ static int intcmp(struct coredump_section *e1, struct coredump_section *e2) {
 
 RB_GENERATE(section_tree, coredump_section, entry, intcmp)
 
-#ifndef HEXDUMP_COLS
-#define HEXDUMP_COLS 16
-#endif
-#include <ctype.h>
-void hexdump(void *mem, unsigned int len) {
-  unsigned int i, j;
-
-  for (i = 0;
-       i <
-       len + ((len % HEXDUMP_COLS) ? (HEXDUMP_COLS - len % HEXDUMP_COLS) : 0);
-       i++) {
-    /* print offset */
-    if (i % HEXDUMP_COLS == 0) {
-      printf("0x%06x: ", i);
-    }
-
-    /* print hex data */
-    if (i < len) {
-      printf("%02x ", 0xFF & ((char *)mem)[i]);
-    } else { /* end of block, just aligning for ASCII dump */
-      printf("   ");
-    }
-
-    /* print ASCII dump */
-    if (i % HEXDUMP_COLS == (HEXDUMP_COLS - 1)) {
-      for (j = i - (HEXDUMP_COLS - 1); j <= i; j++) {
-        if (j >= len) { /* end of block, not really printing */
-          putchar(' ');
-        } else if (isprint(((char *)mem)[j])) { /* printable char */
-          putchar(0xFF & ((char *)mem)[j]);
-        } else { /* other char */
-          putchar('.');
-        }
-      }
-      putchar('\n');
-    }
-  }
-}
-
 int main(int argc, char *argv[]) {
   int ret;
 
